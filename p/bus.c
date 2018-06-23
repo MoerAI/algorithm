f745d908d449c709f4376fda9847222dafd5c089
ToToKr <friendnt@naver.com>
2018-06-23 19:51:28 +0900

    same

--- p/bus.c
+++ p/bus.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int Answer;
 
+int main(void)
+{
+	int T, Answer, test_case;
+	unsigned long K;
+	long id [50];
+    scanf("%d", &T);
+	for(test_case = 0; test_case < T; test_case++)
+	{
+        printf("Case #%d\n", test_case+1);
+        printf("%d\n", Answer);
+	}
+return 0;
+}
