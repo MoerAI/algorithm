3b9623e8203e3305d65c20e5464c311fa23040cf
ToToKr <skyinbss@gmail.com>
2018-09-18 23:35:07 +0900

    에라토스테네스의 체 1000까지 만들기 예제

---
+++ eclipse-workspace/example/src/dyjava/ch4/EratosthenesSieve1.java
@@ -0,0 +1,14 @@
+package dyjava.ch4;
+
+public class EratosthenesSieve1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] number = new int[1000];
+		int [] prime = new int[200];
+		int i, j=0;
+		for(i=0;i<number.length;i++) {
+			number[i] = i+1;
+		}
+	}
+}
