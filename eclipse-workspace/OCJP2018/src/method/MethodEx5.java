05da9d116bdd10a7241e7e54d7f20eaaaaf46597
ToToKr <skyinbss@gmail.com>
2018-10-10 21:59:41 +0900

    static과 non-static에 대한 예제

--- eclipse-workspace/OCJP2018/src/method/MethodEx5.java
+++ eclipse-workspace/OCJP2018/src/method/MethodEx5.java
@@ -1,13 +1,24 @@
 package method;
 
+//call by reference
 public class MethodEx5 {
 	public static void view(String str) {
 		System.out.println(str);
+	}
+	public static void view2(int[] num) {
+		for(int i:num) {
+			System.out.println(i);
+		}
+//		for(int i =0; i<num.length;i++) {
+//			System.out.println(num[i]);
+//		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str ="happy";
 		view(str);
+		
+		int[] num = {10,20,30,40,50};
+		view2(num);
 	}
-
 }
---
+++ eclipse-workspace/OCJP2018/src/method/MethodEx6.java
@@ -0,0 +1,18 @@
+package method;
+
+//인스턴트메서드와 클래스 메서드의 비교
+public class MethodEx6 {
+	public static void view1() {
+		System.out.println("static method");
+	}
+	public void view2() {
+		System.out.println("non-static method");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		view1();//메모리가 계속해서 잡혀있다.
+		
+		MethodEx6 ob = new MethodEx6();//ob를 스텍영역에 선언 후
+		ob.view2();//힙에 있는 view2를 참조한다.
+	}
+}
