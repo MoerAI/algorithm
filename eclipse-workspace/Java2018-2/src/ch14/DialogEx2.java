3c1d88a410f008830d20c903c007ea8a53e93eaa
ToToKr <skyinbss@gmail.com>
2018-11-19 11:36:02 +0900

    dialog 예제

---
+++ eclipse-workspace/Java2018-2/src/ch14/DialogEx2.java
@@ -0,0 +1,36 @@
+package ch14;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class DialogEx2 extends JFrame{
+	MyModalDialog dialog;
+	
+	public DialogEx2() {
+		super("DialogEx 예제프레임");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		dialog = new MyModalDialog(this,"Test Modal Dialog");
+		JButton btn = new JButton("show Modal Dialog");
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dialog.setVisible(true);
+				
+				String text = dialog.getInput();
+				if(text == null)return;
+				
+				JButton btn = (JButton)e.getSource();
+				btn.setText(text);
+			}
+		});
+		getContentPane().add(btn);
+		setSize(250,200);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new DialogEx2();
+	}
+}
---
+++ eclipse-workspace/Java2018-2/src/ch14/MyModalDialog.java
@@ -0,0 +1,36 @@
+package ch14;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class MyModalDialog extends JDialog{
+	JTextField tf = new JTextField(10);
+	JButton okButton = new JButton("OK");
+	
+	public MyModalDialog(JFrame frame, String title) {
+		setLayout(new FlowLayout());
+		add(tf);
+		add(okButton);
+		setSize(200,100);
+		
+		okButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				setVisible(true);
+			}
+		});
+	}
+	
+	String getInput() {
+		if(tf.getText().length()==0)return null;
+		else return tf.getText();
+	}
+}
