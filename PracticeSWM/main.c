abb2bd5621a7c05825ab06ff3179cd46cf9f355b
moerai <friendnt@naver.com>
2019-03-20 17:53:09 +0900

    간단히 선택정렬 만들어봄

--- PracticeSWM/main.c
+++ PracticeSWM/main.c
@@ -3,6 +3,30 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int index, mini, temp;
+    int array[10] = {3, 5, 6, 7, 8, 2, 10, 1, 4, 9};
+
+    for(int i = 0; i < 10; i++)
+    {
+        mini = 99999;
+
+        for(int j = i; j < 10; j++)
+        {
+            if(mini>array[j])
+            {
+                mini = array[j];
+                index = j;
+            }
+        }
+        temp = array[i];
+        array[i] = array[index];
+        array[index] = temp;
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d ",array[i]);
+    }
+
     return 0;
 }
