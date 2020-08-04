b386ae2258a99fd875064ff917be56f002c061bb
박성수 <friendnt@naver.com>
2020-08-04 15:04:21 +0000

    2번풀이

--- PythonAlgorithmInterview/Unit1/palindrome.py.py
+++ PythonAlgorithmInterview/Unit1/palindrome.py.py
@@ -1,8 +1,28 @@
-strs = []
-for char in s:
-    if char.isalnum():
-        strs.append(char.lower())
+#1번풀이
+def isPalindrome(self, s: str) -> bool:
+    #대소문자 전처리
+    strs = []
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+
+    #팰린프롬 여부를 판별        
+    while len(strs) > 1:
+        if strs.pop(0) != strs.pop():
+            return False
+
+#2번풀이
+def isPalindrome(self, s: str) -> bool:
+    #자료형 데크로 선언
+    strs: Deque = collections.deque()
         
-while len(strs) > 1:
-    if strs.pop(0) != strs.pop():
-        return False
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+            
+    while len(strs) > 1:
+        if strs.popleft() != strs.pop():
+            return False
+        
+    return True
+
