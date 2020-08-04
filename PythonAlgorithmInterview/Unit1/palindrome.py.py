a54195db9b0b50320da525ace6f3700f1a81837b
박성수 <friendnt@naver.com>
2020-08-04 15:06:47 +0000

    3번풀이

--- PythonAlgorithmInterview/Unit1/palindrome.py.py
+++ PythonAlgorithmInterview/Unit1/palindrome.py.py
@@ -26,3 +26,10 @@ def isPalindrome(self, s: str) -> bool:
         
     return True
 
+#3번풀이
+def isPalindrome(self, s: str) -> bool:
+    s = s.lower()
+    #정규식으로 불필요한 문자 필터링
+    s = re..sub('[^a-z0-9]', '',s)
+    
+    return s == s[::-1] #슬라이싱
