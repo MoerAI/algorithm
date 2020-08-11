997ed0a6b4f020cd05f6ac4f924502e41585dcdb
박성수 <friendnt@naver.com>
2020-08-11 11:25:07 +0000

    Group Anagrams problem solved

---
+++ PythonAlgorithmInterview/Unit1/group_anagrams.py
@@ -0,0 +1,9 @@
+#leetcode 49 : Group Anagrams
+# 문자열 배열을 받아 애너그램 단위로 그룹핑하라.
+
+#input  : ["eat", "tea", "ate", "nat", "bat"]
+#output : [
+#            ["ate", "eat", "tea"],
+#            ["nat", "tan"],
+#            ["bat"]
+#          ]
