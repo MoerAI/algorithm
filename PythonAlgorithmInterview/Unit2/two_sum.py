62288acc7ce9c53272d18d13d0ed5c13190af9c2
박성수 <friendnt@naver.com>
2020-08-12 15:03:23 +0000

    Tow Sum another solution

--- PythonAlgorithmInterview/Unit2/two_sum.py
+++ PythonAlgorithmInterview/Unit2/two_sum.py
@@ -10,3 +10,10 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
             if nums[i] + num[j] == target:
                 return [i, j]
             
+#2. in을 이용한 탐색
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    for i, n in enumerate(nums):
+        complement = target - n
+
+        if complement in nums[i + 1:]:
+            return nums.index(n), nums[i + 1:].index(complement) + (i + 1)
