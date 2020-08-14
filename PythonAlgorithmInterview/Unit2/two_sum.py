cfb4068c55e72b238944367d0f23b2bb9a1b1015
박성수 <friendnt@naver.com>
2020-08-14 00:42:03 +0000

    speed up the algorithm

--- PythonAlgorithmInterview/Unit2/two_sum.py
+++ PythonAlgorithmInterview/Unit2/two_sum.py
@@ -17,3 +17,16 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         if complement in nums[i + 1:]:
             return nums.index(n), nums[i + 1:].index(complement) + (i + 1)
+        
+#3. 첫 번째 수를 뺀 결과 키 조회
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    nums_map = {}
+    #키와 값을 바꿔서 딕셔너리로 저장
+    for i, num in enumerate(nums):
+        nums_map[num] = i 
+        
+    #타겟에서 첫 번째 수를 뺀 결과를 키로 조회
+    for i, num in enumerate(nums):
+        if target - num in nums_map and i !=nums_map[target - num]:
+            return nums.index(num), nums_map[target - num]
+
