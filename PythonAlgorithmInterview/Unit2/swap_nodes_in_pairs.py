737ee3d4b21d2aad94ffe896072e343bfacd17e1
박성수 <friendnt@naver.com>
2020-08-26 13:28:43 +0000

    use recursive function to solve problem

--- PythonAlgorithmInterview/Unit2/swap_nodes_in_pairs.py
+++ PythonAlgorithmInterview/Unit2/swap_nodes_in_pairs.py
@@ -32,3 +32,12 @@ def swapPairs(self, head: ListNode) -> ListNode:
 
     return root.next
 
+#3. 제귀 구조로 스왑
+def swapPairs(self, head: ListNode) -> ListNode:
+    if head and head.next:
+        p = head.next
+        # 스왑된 값 리턴 받음
+        head.next = self.swapPairs(p.next)
+        p.next = head
+        return p
+    return head
