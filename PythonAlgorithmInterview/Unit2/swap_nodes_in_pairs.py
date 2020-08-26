b20d3ef73fe8ae5312239155612a96ca4b4ede4c
박성수 <friendnt@naver.com>
2020-08-26 13:27:48 +0000

    use while to solve problem

--- PythonAlgorithmInterview/Unit2/swap_nodes_in_pairs.py
+++ PythonAlgorithmInterview/Unit2/swap_nodes_in_pairs.py
@@ -14,3 +14,21 @@ def swapPairs(self, head: ListNode) -> ListNode:
 
     return head
 
+#2. 반복 구조로 스왑
+def swapPairs(self, head: ListNode) -> ListNode:
+    root = prev = ListNode(None)
+    prev.next = head
+    while head and head.next:
+        b = head.next
+        head.next = b.next
+        b.next = head
+
+        # prev가 b를 가리키도록 할당
+        prev.next = b
+
+        # 다음번 비교를 위해 이동
+        head = head.next
+        prev = prev.next.next
+
+    return root.next
+
