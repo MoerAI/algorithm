0fd0aabf32bf77a16fec7bee4766458209c7672b
박성수 <friendnt@naver.com>
2020-08-30 09:31:16 +0000

    change file name

---
+++ PythonAlgorithmInterview/Unit2/array_partion1.py
@@ -0,0 +1,35 @@
+#leetcode 561 : Array Partition 1
+#n개의 페어를 이용한 min(a, b)의 합으로 만들 수 있는 가장 큰 수를 출력하라.
+#input  : nums = [1, 4, 3, 2]
+#output : 4
+
+#오름차순으로 정렬 한 후 계산
+def arrayPairSum(self, nums: List[int]) -> int:
+    sum = 0
+    pair = []
+    nums.sort()
+
+    for n in nums:
+        # 앞에서부터 오름차순으로 페어를 만들어서 합 계산
+        pair.append(n)
+        if len(pair) == 2:
+            sum += min(pair)
+            pair = []
+
+    return sum
+
+#짝수 번째 값 계산
+def arrayPairSum(self, num: List[int]) -> int:
+    sum = 0
+    nums.sort()
+
+    for i, n in enumerate(nums):
+        if i % 2 == 0:
+            sum += n
+
+    return sum
+
+#파이썬다운 방식
+
+def arrayPairSum(self, nums: List[int]) -> int:
+    return sum(sorted(nums)[::2])
