7209bb6f0c439e6a6f7abbc682095c6b68baec72
박성수 <friendnt@naver.com>
2020-09-29 03:27:13 +0000

    add ListNode class and modify some code to correct grammar

--- PythonAlgorithmInterview/Unit2/design_hashmap.py
+++ PythonAlgorithmInterview/Unit2/design_hashmap.py
@@ -4,22 +4,24 @@
 # - put(key, value): 키, 값을 해시맵에 삽입한다. 만약 이미 존재하는 키라면 업데이트한다.
 # - get(key, value): 키에 해당하는 값을 조회한다. 만약 키가 존재하지 않는다면 -1을 리턴한다.
 # - remove(key): 키에 해당하는 키, 값을 해시맵에서 삭제한다.
+import collections
 
+
+class ListNode:
+    def __init__(self, key = None, value = None):
+        self.key = key
+        self.value = value
+        self.next = None
+        
 class MyHashMap:
 
     #초기화
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.size = 1000
         self.table = collections.defaultdict(ListNode)
     
     # 삽입
     def put(self, key: int, value: int) -> None:
-        """
-        value will always be non-negative.
-        """
         index = key % self.size
         # 인덱스에 노드가 없다면 삽입 후 종료
         if self.table[index].value is None:
@@ -39,19 +41,26 @@ class MyHashMap:
 
     # 조회
     def get(self, key: int) -> int:
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        """
         index = key % self.size
         if self.table[index].value is None:
-            return
+            return -1
+        
+        # 노드가 존재할 때 일치하는 키 탐색
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                return p.value
+            p = p.next
+        return -1
 
     # 삭제
     def remove(self, key: int) -> None:
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        """
         index = key % self.size
+        if self.table[index].value is None:
+            return
+            
+        # 인덱스의 첫 번째 노드일 때 삭제 처리
+        p = self.table[index]
         if p.key == key:
             self.table[index] = ListNode() if p.next is None else p.next
             return
@@ -63,7 +72,6 @@ class MyHashMap:
                 prev.next = p.next
                 return
             prev, p = p, p.next
-
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
