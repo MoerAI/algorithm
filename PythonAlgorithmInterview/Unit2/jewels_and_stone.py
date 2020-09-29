16d846b5da13080bf0b80e774f780c61e62ffa4b
박성수 <friendnt@naver.com>
2020-09-29 13:26:31 +0000

    solve problem using defaultdict

--- PythonAlgorithmInterview/Unit2/jewels_and_stone.py
+++ PythonAlgorithmInterview/Unit2/jewels_and_stone.py
@@ -2,6 +2,8 @@
 # J는 보석이며, S는 보석을 갖고 있는 돌이다. S에는 보석이 몇 개나 있을까? 대소문자는 구분한다.
 # input  : J = "aA", S = "aAAbbbbb"
 # output : 3
+import collections
+
 
 class Solution:
     # 해시 테이블을 이용한 풀이
@@ -22,4 +24,17 @@ class Solution:
                 
         return count
     
-    
+    # defaultdict을 이용한 비교 생략
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        freqs = collections.defaultdict(int)
+        count = 0
+        
+        # 비교 없이 돌(S) 빈도 수 계산
+        for char in S:
+            freqs[char] += 1
+            
+        # 비교 없이 보석(J) 빈도 수 합산
+        for char in J:
+            count += freqs[char]
+            
+        return count
