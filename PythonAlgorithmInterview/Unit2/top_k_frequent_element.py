f44c1e7092ac30b6e13d38e48c577e404cc1b197
박성수 <friendnt@naver.com>
2020-10-02 13:46:06 +0000

    problem solve using heapq, counter

---
+++ PythonAlgorithmInterview/Unit2/top_k_frequent_element.py
@@ -0,0 +1,18 @@
+# leetcoe 347 : Top K Frequent Elements
+# k번 이상 등장하는 요소를 추출하라.
+# input  : nums = [1,1,1,2,2,3], k = 2
+# output : [1,2]
+
+#Counter를 이용한 음수 순 추출
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    freqs = collections.Counter(nums)
+    freqs_heap = []
+    # 힙에 음수로 삽입
+    for f in freqs:
+        heapq.heappush(freqs_heap, (-freqs[f], f))
+    topk = list()
+    for _ in range(k):
+        topk.append(heapq.heappop(freqs_heap)[1])
+
+    return topk
+
