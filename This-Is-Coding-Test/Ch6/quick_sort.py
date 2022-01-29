5fc737b9b3f611fa0e3f3dd57c743da772f4a2e8
Moerai <friendnt@naver.com>
2022-01-29 17:12:17 +0900

    add another version of quick sort example

--- This-Is-Coding-Test/Ch6/quick_sort.py
+++ This-Is-Coding-Test/Ch6/quick_sort.py
@@ -21,5 +21,23 @@ def quick_sort(array, start, end):
     quick_sort(array, start, right - 1)
     quick_sort(array, right + 1, end)
 
+
 quick_sort(array, 0, len(array) - 1)
 print(array)
+
+array2 = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+def quick_sort_py(array):
+    # 리스트가 하나 이하의 원소만을 담고 있다면 종료
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0] # 피벗은 첫 번째 원소
+    tail = array[1:] # 피벗을 제외한 리스트
+
+    left_side = [x for x in tail if x <= pivot] # 분할된 왼쪽 부분
+    right_side = [x for x in tail if x > pivot] # 분할된 오른쪽 부분
+
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬을 수행하고, 전체 리스트를 반환
+    return quick_sort_py(left_side) + [pivot] + quick_sort_py(right_side)
+
+print(quick_sort_py(array2))
