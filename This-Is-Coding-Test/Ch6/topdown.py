3aa410d5057f4cb422fc19de723c5d16f4dd91d1
Moerai <friendnt@naver.com>
2022-01-29 17:38:57 +0900

    change topdown problem

--- This-Is-Coding-Test/Ch6/topdown.py
+++ This-Is-Coding-Test/Ch6/topdown.py
@@ -1,11 +1,10 @@
-# import pytest
-#
-# @pytest.mark.parametrize(
-#     "num, array, expected",
-#     [(3, [15, 27, 12]],
-#       [27, 15, 12]
-#       )]
-# )
+import pytest
+
+@pytest.mark.parametrize(
+    'count, inputs, expected',
+    [(3,
+      [15, 27, 12],
+      [27, 15, 12])])
 def test_solve(num, array, expected):
     result = solve(num, array)
     assert result == expected
