1344d13c4e471ca6e9a53e9c4ad4f98c6557975d
Moerai <skyinbss@gmail.com>
2022-03-04 15:42:56 +0900

    solve devide city problem

--- This-Is-Coding-Test/Ch10/make_team.py
+++ This-Is-Coding-Test/Ch10/make_team.py
@@ -5,6 +5,7 @@ def find_parent(parent, x):
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
+
 # 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
@@ -13,6 +14,7 @@ def union_parent(parent, a, b):
         parent[b] = a
     else:
         parent[a] = b
+
 
 n, m = map(int, input().split())
 parent = [0] * (n + 1) # 부모 테이블 초기화
