0292d62ac0fe92b6dbfbf727f02351308737b97a
Moerai <skyinbss@gmail.com>
2022-03-23 14:23:20 +0900

    solve kor_eng_math problem

---
+++ This-Is-Coding-Test/Ch14/Kor_Eng_Math.py
@@ -0,0 +1,43 @@
+import pytest
+@pytest.mark.parametrize(
+    'n, m, k, x, array, expected',
+    [
+        12,
+        [("Junkyu", 50, 60, 100),
+         ("Sangkeun", 80, 60, 50),
+         ("Sunyoung", 80, 70, 100),
+         ("soong", 50, 60, 90),
+         ("haebin", 50, 60, 100),
+         ("Kangsoo", 60, 80, 100),
+         ("Donghyuk", 80, 60, 100),
+         ("Sei", 70, 70, 70),
+         ("Wonseob", 70, 70, 90),
+         ("Sanghyun", 70, 70, 80),
+         ("nsj", 80, 80, 80),
+         ("Taewhan", 50, 60, 90)
+        ],
+        ["Donghyuk",
+         "Sangkeun",
+         "Sunyoung",
+         "nsj",
+         "Wonseob",
+         "Sanghyun",
+         "Sei",
+         "Kangsoo",
+         "Junkyu",
+         "haebin",
+         "soong",
+         "Taewhan"
+        ]
+    ]
+)
+def test(n, array, expected):
+    result = solution(n, array)
+    assert result == expected
+
+def solution(n, array):
+    array.sort(key=lambda x : (-x[1], x[2], -x[3]))
+    answer = []
+    for name, kor, eng, math in array:
+        answer.append(name)
+    return answer
