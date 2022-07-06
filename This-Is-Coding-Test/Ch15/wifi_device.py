2a1c22250d6e4a93effae42f5b87a66a30463b69
Moerai <skyinbss@gmail.com>
2022-07-06 18:12:36 +0900

    Change wifi_device Solution

--- This-Is-Coding-Test/Ch15/wifi_device.py
+++ This-Is-Coding-Test/Ch15/wifi_device.py
@@ -7,8 +7,22 @@ for _ in range(n):
      array.append(int(input()))
 array.sort()
 
-dis = array[-1] - array[0]
+start = array[1] - array[0]
+end = array[-1] - array[0]
+result = 0
 
-print(dis)
+while(start <= end):
+     mid = (start + end) // 2
+     value = array[0]
+     count = 1
+     for i in range(1, n):
+          if array[i] >= value + mid:
+               value = array[i]
+               count += 1
+     if count >= c:
+          start = mid + 1
+          result = mid
+     else:
+          end = mid - 1
 
-print(math.ceil(dis/c))
+print(result)
