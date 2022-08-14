34a9ceff593734ff8cfaa1db9eb48717134b461a
Moerai <skyinbss@gmail.com>
2022-08-15 08:47:43 +0900

    동일한 단어 그룹화하기

---
+++ acmicpc/16499.py
@@ -0,0 +1,49 @@
+# Problem
+"""
+소문자로 이루어진 단어 N개가 주어졌을 때, 단어가 총 최소 몇 개의 그룹으로 이루어져 있는지 구하는 프로그램을 작성하시오.
+그룹에 속한 단어는 모두 같은 알파벳으로 이루어져 있어야 하고, 개수도 같아야 한다. 즉, 단어를 구성하는 알파벳의 순서만 달라야 한다.
+"""
+
+# Input
+"""
+첫째 줄에 단어의 개수 N이 주어진다.
+(2 ≤ N ≤ 100) 둘째 줄부터 N개의 줄에 단어가 한 줄에 하나씩 주어진다.
+단어는 알파벳 소문자로만 이루어져 있고, 길이는 10을 넘지 않는다.
+"""
+
+# Output
+"""
+첫째 줄에 그룹의 최소 개수를 출력한다.
+"""
+
+# Example
+"""
+|Input1|
+5
+cat
+dog
+god
+tca
+gcc
+|Output1|
+3
+
+|Input2|
+2
+a
+a
+|Output2|
+1
+"""
+# Soultion
+n = int(input())
+result = 0
+words = []
+for _ in range(n):
+    s = sorted(list(input()))
+    s = str(s)
+    words.append(s)
+
+words = set(words)
+
+print(len(words))
